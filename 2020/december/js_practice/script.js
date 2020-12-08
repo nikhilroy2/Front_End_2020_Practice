3b7519a74f0d2d1557718b8ce03df2b98c32e324
FENR1 USER <fenr1user@biadsconsulting.org>
2020-12-08 10:14:22 +0600

    js practice repeating

--- 2020/december/js_practice/script.js
+++ 2020/december/js_practice/script.js
@@ -1,14 +1,9 @@
-// Function to get the Selected Text  
-function getSelectedText() { 
-    var selectedText = ''; 
+function textSelection(){
+    let newInput = document.createElement('input');
+    newInput.setAttribute('value', window.getSelection());
+    console.log(newInput.value)
+    document.body.appendChild(newInput);
+    newInput.remove()
 
-    // window.getSelection 
-    if (window.getSelection) { 
-        selectedText = window.getSelection(); 
-    } 
-    
-     else return; 
-    // To write the selected text into the textarea 
-    document.testform.selectedtext.value = selectedText
-} 
-document.onmouseup = getSelectedText
+}
+document.onmouseup = textSelection
