2366b4d85a02540916e3504b5ba744706d4c7d0f
FENR1 USER <fenr1user@biadsconsulting.org>
2020-12-10 22:36:04 +0600

    jquery

--- 2020/december/es6_practice/dec7.js
+++ 2020/december/es6_practice/dec7.js
@@ -18,3 +18,33 @@ console.log([...txt].splice(3))
 let name = 'Linkedin'
 console.log([...name].toString()) // es6
 console.log(name.split('').toString())// es5
+
+
+function quux (strings, ...values) {
+    strings[0] === "foo\n"
+    strings[1] === "bar"
+    strings.raw[0] === "foo\\n"
+    strings.raw[1] === "bar"
+    values[0] === 42
+}
+quux`foo\n${ 42 }bar`
+console.log()
+String.raw`foo\n${ 42 }bar` === "foo\\n42bar"
+console.log(0b111110111)
+
+
+let person = ['nikhil', 'sonchoy',30,40];
+person.sort().splice(4)
+let p_result = person.map((name,i)=> ({name}))
+console.log(p_result)
+
+
+{
+    const quux = ()=> "b"
+let obj_1 = {
+    name: "nikhil",
+    ["a"+quux()] : "Array"
+}
+
+console.log(obj_1.ab);
+}
