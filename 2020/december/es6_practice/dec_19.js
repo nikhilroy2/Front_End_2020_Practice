a213ae1dea08c9baf80cc470b6f2e9f2fb241468
FENR1 USER <fenr1user@biadsconsulting.org>
2020-12-21 15:31:00 +0600

    es6 practice dec_19.js

---
+++ 2020/december/es6_practice/dec_19.js
@@ -0,0 +1,13 @@
+// let ar = [2,3,54,5,34,65]
+// let result = ar.find((a,b,c)=> console.log(a))
+// result;
+
+// let r = "hello".startsWith("ello", 4)
+// console.log(r)
+
+let h = "Hello World"
+
+//console.log(h.includes("World"))
+// console.log(h.startsWith("He",0))
+// console.log(h.endsWith("World", h.length))
+console.log(h.includes("Hello",0))
