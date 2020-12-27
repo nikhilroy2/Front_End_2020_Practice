be59dc3c8c2c731c4b1f6622dc7b370261031a1e
FENR1 USER <fenr1user@biadsconsulting.org>
2020-12-27 16:23:59 +0600

    js practice

--- 2020/december/js_practice/dec13.js
+++ 2020/december/js_practice/dec13.js
@@ -2,4 +2,4 @@ let text = "How are you? ||| তুমি কেমন আছো? "
 
 let en = text.slice(0,text.indexOf('|||'));
 let bn = text.slice(text.indexOf('|||') + 4, text.length)
-console.log(bn)
+console.log(en)
